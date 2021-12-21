161373bf3117bf4745622c2b24a1655b6c8c42b3
luthfisolahudin <54571802+luthfisolahudin@users.noreply.github.com>
2021-12-22 02:10:25 +0700

    main class

--- src/main/java/id/luthfisolahudin/smkn4/pbo/todolist/Main.java
+++ src/main/java/id/luthfisolahudin/smkn4/pbo/todolist/Main.java
@@ -1,7 +1,9 @@
 package id.luthfisolahudin.smkn4.pbo.todolist;
 
+import id.luthfisolahudin.smkn4.pbo.todolist.cli.controller.TodoController;
+
 public class Main {
     public static void main(String[] args) {
-
+        TodoController.index();
     }
 }
