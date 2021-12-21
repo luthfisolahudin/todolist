109ca1b8d14404f4ea34b52d097d927177d0b248
luthfisolahudin <54571802+luthfisolahudin@users.noreply.github.com>
2021-12-21 17:06:04 +0700

    update SQLiteHelper: add method createColumn

--- src/main/java/id/luthfisolahudin/smkn4/pbo/todolist/core/helper/SQLiteHelper.java
+++ src/main/java/id/luthfisolahudin/smkn4/pbo/todolist/core/helper/SQLiteHelper.java
@@ -1,6 +1,10 @@
 package id.luthfisolahudin.smkn4.pbo.todolist.core.helper;
 
+import com.alexfu.sqlitequerybuilder.api.Column;
+import com.alexfu.sqlitequerybuilder.api.ColumnConstraint;
+import com.alexfu.sqlitequerybuilder.api.ColumnType;
 import id.luthfisolahudin.smkn4.pbo.todolist.core.Configuration;
+import id.luthfisolahudin.smkn4.pbo.todolist.core.common.ColumnConfigurationKey;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -9,6 +13,7 @@ import org.apache.logging.log4j.Level;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Map;
 
 @Log4j2
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
@@ -39,5 +44,20 @@ public class SQLiteHelper {
         }
 
         return null;
+    }
+
+    public static Column createColumn(Map<ColumnConfigurationKey, Object> columnConfiguration) {
+        String name = (String) columnConfiguration.get(ColumnConfigurationKey.NAME);
+        ColumnType type = (ColumnType) columnConfiguration.get(ColumnConfigurationKey.TYPE);
+        ColumnConstraint constraint = (ColumnConstraint) columnConfiguration.getOrDefault(
+                ColumnConfigurationKey.CONSTRAINT,
+                null
+        );
+        String defaultValue = (String) columnConfiguration.getOrDefault(
+                ColumnConfigurationKey.DEFAULT_VALUE,
+                null
+        );
+
+        return new Column(name, type, constraint, defaultValue);
     }
 }
