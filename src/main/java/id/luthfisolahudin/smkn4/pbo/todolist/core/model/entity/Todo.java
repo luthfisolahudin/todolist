f6e95a74fa17731a25d6654854e87568dc71cd68
luthfisolahudin <54571802+luthfisolahudin@users.noreply.github.com>
2021-12-21 17:03:26 +0700

    update entity todo: add logger, and update attribute (add @With and @Builder.Default)

--- src/main/java/id/luthfisolahudin/smkn4/pbo/todolist/core/model/entity/Todo.java
+++ src/main/java/id/luthfisolahudin/smkn4/pbo/todolist/core/model/entity/Todo.java
@@ -3,22 +3,29 @@ package id.luthfisolahudin.smkn4.pbo.todolist.core.model.entity;
 import id.luthfisolahudin.smkn4.pbo.todolist.core.model.Description;
 import id.luthfisolahudin.smkn4.pbo.todolist.core.model.Name;
 import lombok.*;
+import lombok.extern.log4j.Log4j2;
 
 @AllArgsConstructor
 @Builder
 @Data
+@Log4j2
 public class Todo {
     @Builder.Default
     final TodoId id = null;
 
     @NonNull
+    @With
     Name name;
 
+    @Builder.Default
     @NonNull
-    Description description;
+    @With
+    Description description = Description.of(null);
 
+    @Builder.Default
     @NonNull
-    TodoStatus status;
+    @With
+    TodoStatus status = TodoStatus.PENDING;
 
     @Value(staticConstructor = "of")
     public static class TodoId {
