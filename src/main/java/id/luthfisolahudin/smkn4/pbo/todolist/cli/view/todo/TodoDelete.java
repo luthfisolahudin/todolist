539f5297fb6f910acbac72b367cef06ac1fcbef5
luthfisolahudin <54571802+luthfisolahudin@users.noreply.github.com>
2021-12-30 15:54:34 +0700

    update view delete todo: remove showing todo id and add some new line

--- src/main/java/id/luthfisolahudin/smkn4/pbo/todolist/cli/view/todo/TodoDelete.java
+++ src/main/java/id/luthfisolahudin/smkn4/pbo/todolist/cli/view/todo/TodoDelete.java
@@ -13,14 +13,20 @@ public final class TodoDelete {
     private static final Scanner input = new Scanner(System.in);
 
     public static Map<String, String> display(Todo data) {
-        System.out.println("ID: " + data.getId().getValue());
-        System.out.println("Nama: " + data.getName().getValue());
-        System.out.println("Deskripsi: " + data.getDescription().getValue());
-        System.out.println("Status: " + StringUtils.capitalize(data.getStatus().toString()));
+        String name = data.getName().getValue();
+        String description = data.hasDescription() ? data.getDescription().getValue() : "-";
+        String status = StringUtils.capitalize(data.getStatus().toString().toLowerCase());
+
+        System.out.println("Nama: " + name);
+        System.out.println("Deskripsi: " + description);
+        System.out.println("Status: " + status);
+        System.out.println();
         System.out.println("[list] Kembali ke daftar Todo");
         System.out.println("[back] Kembali ke detail Todo");
+        System.out.println();
         System.out.print("Apakah Anda yakin ingin menghapus item ini? (Y/n) ");
         String inputChoice = input.nextLine();
+        System.out.println();
 
         return Map.ofEntries(
                 Map.entry("choice", inputChoice)
