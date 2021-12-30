a7d4e7937ba7a051d2f2ff2bba66dc17017f4981
luthfisolahudin <54571802+luthfisolahudin@users.noreply.github.com>
2021-12-30 15:47:25 +0700

    update view error input invalid: separate each method and add new line at the end

--- src/main/java/id/luthfisolahudin/smkn4/pbo/todolist/cli/view/error/InputInvalid.java
+++ src/main/java/id/luthfisolahudin/smkn4/pbo/todolist/cli/view/error/InputInvalid.java
@@ -7,15 +7,18 @@ import lombok.NoArgsConstructor;
 public final class InputInvalid {
     public static void display() {
         System.out.println("Input yang Anda masukan tidak sesuai.");
+        System.out.println();
     }
 
     public static void display(Integer min, Integer max) {
-        display();
+        System.out.println("Input yang Anda masukan tidak sesuai.");
         System.out.printf("Silakah masukan angka di antara %d dan %d.\n", min, max);
+        System.out.println();
     }
 
     public static void display(String positive, String negative) {
-        display();
+        System.out.println("Input yang Anda masukan tidak sesuai.");
         System.out.printf("Silakah masukan antara %s dan %s.\n", positive, negative);
+        System.out.println();
     }
 }
